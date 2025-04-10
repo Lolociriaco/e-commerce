ffa243992375c5b64e5919df3fe849b8f333cf90
Lorenzo <ciriacolorenzo5@gmail.com>
2025-04-10 19:32:52 -0300

    changing grid item link

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -14,24 +14,21 @@ export const ProductGridItem = ({ product }: Props) => {
     const [displayImage, setdisplayImage] = useState( product.images[0])
 
   return (
-    <div className='rounded-sm overflow-hidden fade-in'>
+    <Link className='rounded-sm overflow-hidden fade-in group'
+    href={`/product/${ product.slug }`}>
         <Image
             src={`/products/${displayImage}`}
             alt={product.title}
-            className='w-full object-cover rounded'
+            className='w-full object-cover rounded transition-opacity duration-300'
             width={ 500 }
             height={ 500 }
             onMouseEnter={() => setdisplayImage( product.images[1])}
             onMouseLeave={() => setdisplayImage( product.images[0])}
         />
         <div className='p-2 flex flex-col'>
-            <Link
-                className='hover:text-blue-950' 
-                href={`/product/${ product.slug }`}>
-                { product.title }
-            </Link>
+            <span className='group-hover:text-blue-950 font-semibold'>{ product.title }</span>
             <span className='text-md font-bold'>${ product.price }</span>
         </div>
-    </div>
+    </Link>
   )
 }
